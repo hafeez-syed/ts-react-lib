5cd3ccd46465ad47fa1fe59353e4180b7ef97077
Hafeez <feeziman007@hotmail.com>
2018-10-25 00:07:07 +1100

    lib target modified

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,8 +3,16 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     output: {
-        library: 'OneReactComponents',
-        libraryTarget: 'window',
+        //library: 'OneReactComponents',
+	    library: {
+		    //global: 'OneReactComponents',
+		    root: 'OneReactComponents',
+		    //this: 'OneReactComponents',
+		    //window: 'OneReactComponents',
+		    amd: 'one-react-components',
+		    commonjs: 'one-react-components'
+	    },
+        libraryTarget: 'umd',
         filename: 'one-react-components.js'/*,
         umdNamedDefine: true*/
     },
