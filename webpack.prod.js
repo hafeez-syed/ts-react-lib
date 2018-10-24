675623d6ccbd61fd5e729cd476a842d0a83a5041
Hafeez <feeziman007@hotmail.com>
2018-10-25 00:28:57 +1100

    lib changed to umd

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,15 +3,15 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     output: {
-        //library: 'OneReactComponents',
-	    library: {
+        library: 'oneReactComponents',
+	    /*library: {
 		    //global: 'OneReactComponents',
 		    root: 'OneReactComponents',
 		    //this: 'OneReactComponents',
 		    //window: 'OneReactComponents',
 		    amd: 'one-react-components',
 		    commonjs: 'one-react-components'
-	    },
+	    },*/
         libraryTarget: 'umd',
         filename: 'one-react-components.js'/*,
         umdNamedDefine: true*/
